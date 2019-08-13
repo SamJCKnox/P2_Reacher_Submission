bd3565f41ba142a74ce9c358af5499ba9825a23e
SamJCKnox <47692324+SamJCKnox@users.noreply.github.com>
2019-08-13 16:35:50 +0100

    Normalisation Layer Added and Failed

--- MultiAgent/model.py
+++ MultiAgent/model.py
@@ -59,6 +59,7 @@ class Critic(nn.Module):
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(args['random_seed'])
         self.fcs1 = nn.Linear(args['state_size'], args['fc1_units'])
+        self.bn1 = nn.BatchNorm1d(num_features=args['fc1_units'])
         self.fc2 = nn.Linear(args['fc1_units']+args['action_size'], args['fc2_units'])
         self.fc3 = nn.Linear(args['fc2_units'], 1)
         #self.reset_parameters()
@@ -70,7 +71,7 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.relu(self.fcs1(state))
+        xs = F.relu(self.bn1(self.fcs1(state)))
         x = torch.cat((xs, action), dim=1)
         x = F.relu(self.fc2(x))
         return self.fc3(x)
